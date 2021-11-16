f11a22c9e597b2940f3a0266308649086e2409d0
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-11-16 19:58:17 +0000

    P2-42 #comment added delete method to actor controller

--- src/main/java/com/qa/project2/controller/ActorController.java
+++ src/main/java/com/qa/project2/controller/ActorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +43,12 @@ public class ActorController {
 	
 	@PutMapping("/update/{id}")
 	public ResponseEntity<Actor> update(@PathVariable long id, @RequestBody Actor actor){
-		return new ResponseEntity<Actor>(this.service.update(id, actor), HttpStatus.OK);
+		return new ResponseEntity<Actor>(this.service.update(id, actor), HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Actor> delete(long id){
+		return this.service.delete(id)? new ResponseEntity<Actor>(HttpStatus.NO_CONTENT):new ResponseEntity<Actor>(HttpStatus.NOT_FOUND);
 	}
 	
 	
