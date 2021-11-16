9610563824ac75eaad374ba1724530c70744f110
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-11-16 13:03:31 +0000

    P2-23 #comment completed movie service readAllByCast test

--- src/test/java/com/qa/project2/service/MovieServiceUnitTest.java
+++ src/test/java/com/qa/project2/service/MovieServiceUnitTest.java
@@ -134,4 +134,24 @@ public class MovieServiceUnitTest {
 		Mockito.verify(repo, times(1)).findAllByYear();
 	}
 	
+	@Test
+	void readAllByCastTest() {
+		Actor actor1 = new Actor("sample", "actor1");
+		Actor actor2 = new Actor("sample", "actor2");
+		List<Actor> cast = new ArrayList<Actor>();
+		cast.add(actor1);
+		cast.add(actor2);
+
+		Movie movie1 = new Movie(1l, "title", "synopsis", 1990, cast, 3.7);
+		Movie movie2 = new Movie(1l, "title2", "synopsis2", 1990, cast, 4.9);
+		List<Movie> movies = new ArrayList<Movie>();
+		
+		
+		Mockito.when(repo.findAllByCast("sample actor1")).thenReturn(movies);
+
+		Assertions.assertEquals(movies, service.readAllByCast("sample actor1"));
+		
+		Mockito.verify(repo, times(1)).findAllByCast("sample actor1");
+	}
+	
 }
