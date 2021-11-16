1e537eff8504f8fbaa22f4b06a3c7175947e6416
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-11-16 16:44:39 +0000

    P2-75 #comment created sql files for testing

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS `MOVIE`;
+
+CREATE TABLE `MOVIE`
+(
+`id` BIGINT auto_increment,
+`title` varchar(255) NOT NULL,
+`synopsis` varchar(255),
+`year` INT,
+`rating` DECIMAL(2,1),
+`runtime` INT,
+PRIMARY KEY(id)
+)
+
+DROP TABLE IF EXISTS `ACTOR`;
+
+CREATE TABLE `ACTOR`
+(
+`id` BIGINT auto_increment,
+`first_name` varchar(255) NOT NULL,
+`last_name` varchar(255) NOT NULL,
+PRIMARY KEY(id)
+)
+
+DROP TABLE IF EXISTS `MOVIE_CAST`;
+
+CREATE TABLE `MOVIE_CAST`
+(
+`id` BIGINT auto_increment,
+`movie_id` BIGINT NOT NULL,
+`cast_id` BIGINT NOT NULL,
+PRIMARY KEY(id)
+)
