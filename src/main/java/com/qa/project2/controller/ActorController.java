8d24a370726ab9df72ac47488330ba2026a24722
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-11-16 19:54:15 +0000

    P2-41 #comment added update method to actor controller

--- src/main/java/com/qa/project2/controller/ActorController.java
+++ src/main/java/com/qa/project2/controller/ActorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,11 @@ public class ActorController {
 	@GetMapping("/get/{name}")
 	public ResponseEntity<List<Actor>> readAllByName(@PathVariable String name){
 		return new ResponseEntity<List<Actor>>(this.service.readByName(name), HttpStatus.OK);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Actor> update(@PathVariable long id, @RequestBody Actor actor){
+		return new ResponseEntity<Actor>(this.service.update(id, actor), HttpStatus.OK);
 	}
 	
 	
