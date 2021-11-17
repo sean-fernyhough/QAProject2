efd3458581f7a9e2414eda1a332154fa5346eadf
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-11-17 11:08:41 +0000

    P2-53 #comment added create and read all tests

--- src/test/java/com/qa/project2/MovieIntegrationTest.java
+++ src/test/java/com/qa/project2/MovieIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.qa.project2;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -58,8 +59,35 @@ public class MovieIntegrationTest {
 		ResultMatcher checkBody = content().json(createdMovieJson);
 		
 		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
+	}
+	
+	@Test
+	public void readAllTest() throws Exception {
+		Actor actor1 = new Actor(1l, "steven", "seagull", null);
+		Actor actor2 = new Actor(2l, "van", "diesel", null);
+		Actor actor3 = new Actor(3l, "bruce", "willyoueverstopmakingbadmovies", null);
+		List<Actor> cast1 = new ArrayList<Actor>();
+		cast1.add(actor1);
+		cast1.add(actor2);
+		List<Actor> cast2 = new ArrayList<Actor>();
+		cast2.add(actor1);
+		cast2.add(actor3);
 		
+		List<Movie> movies = new ArrayList<Movie>();
+		Movie movie1 = new Movie(1l, "bad movie", 2003, 93,  cast1, "this movie is bad", 2.7);
+		Movie movie2 = new Movie(2l, "worse sequel", 2005, 124,  cast2, "this movie is even worse", 1.3);
+		movies.add(movie1);
+		movies.add(movie2);
 		
+		String movieJson = mapper.writeValueAsString(movies);
+		
+		RequestBuilder request = get("/movies/get").contentType(MediaType.APPLICATION_JSON);
+		
+		ResultMatcher checkStatus = status().is(200);
+		
+		ResultMatcher checkBody = content().json(movieJson);
+		
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
 	}
 
 }
